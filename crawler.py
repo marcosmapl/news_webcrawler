0d61c10c5863a644d0386dcf4ce602e03ce1f2fb
Marcos Lima <marcos.lima@icomp.ufam.edu.br>
2022-07-11 20:13:48 -0400

    files(html): add html articles for key <haitiano>

--- crawler.py
+++ crawler.py
@@ -26,7 +26,7 @@ class AcriticaCrawler:
 
     @staticmethod
     def find_news(terms: List[str]):
-        news_links = []
+        article_links = set()
         browser = webdriver.Chrome()
         browser.maximize_window()
         for term in terms:
@@ -43,43 +43,49 @@ class AcriticaCrawler:
                 sleep(AcriticaCrawler.DEFAULT_LOAD_WAIT_TIME)
                 soup = BeautifulSoup(browser.page_source, 'html.parser')
                 links = soup.findAll('a', {'class': 'dDfqGJ', 'href': True})
-                news_links.extend([str(link['href']) for link in links])
-                break
+                for link in links:
+                    article_url = str(link['href'])
+                    article_links.add(article_url)
+                    print(f"MATERIA ADICIONADA A LISTA DE DOWNLOAD: {article_url}\nTOTAL {len(article_links)}")
         browser.close()
-        return news_links
+        return article_links
 
     @staticmethod
-    def get_articles(articles_urls: List[str]):
+    def get_articles(article_paths: List[str]):
         articles = []
         browser = webdriver.Chrome()
         browser.maximize_window()
-        for articles_url in articles_urls:
-            browser.get(f'{AcriticaCrawler.PORTAL_BASE_URL}{articles_url}')
+        for article_path in article_paths:
+            article_url = f'{AcriticaCrawler.PORTAL_BASE_URL}{article_path}'
+            print(f"BAIXANDO MATERIA: {article_url}")
+            browser.get(article_url)
             sleep(AcriticaCrawler.DEFAULT_LOAD_WAIT_TIME)
             soup = BeautifulSoup(browser.page_source, 'html.parser')
-            article_data = {meta['property']: str(meta['content']).upper().strip() for meta in soup.findAll('meta', {'property': True})}
-            cat_span = soup.find('span', {'class': 'ceiOww'})
-            article_data['article_id'] = 'AC' + article_data['og:url'][article_data['og:url'].rindex('.')+1:]
-            article_data['header'] = cat_span.text.upper().strip() if cat_span else None
-            url_parts = article_data['og:url'].split('/')
-            article_data['editorial'] = str(url_parts[1]).strip().upper() if len(url_parts) > 2 else None
-            tags = soup.findAll('span', {'class': 'gCiqnR'})
+            try:
+                article_data = {meta['property']: str(meta['content']).upper().strip() for meta in soup.findAll('meta', {'property': True})}
+                cat_span = soup.find('span', {'class': 'ceiOww'})
+                article_data['article_id'] = 'AC' + article_data['og:url'][article_data['og:url'].rindex('.')+1:]
+                article_data['header'] = cat_span.text.upper().strip() if cat_span else None
+                url_parts = article_path.split('/')
+                article_data['editorial'] = str(url_parts[0]).strip().upper() if len(url_parts) > 2 else None
+                tags = soup.findAll('span', {'class': 'gCiqnR'})
 
-            if tags:
-                article_data['tags'] = '#'.join([tag.text.strip().upper() for tag in tags])
-            else:
-                article_data['tags'] = None
+                if tags:
+                    article_data['tags'] = '#'.join([tag.text.strip().upper() for tag in tags])
+                else:
+                    article_data['tags'] = None
 
-            article_content = []
-            for div in soup.findAll('div', {'class': 'bVxWXz'}):
-                for element in div.findChildren():
-                    if AcriticaCrawler.tag_visible(element) and not element.findChild() and element.text:
-                        print(element.text)
-                        article_content.append(element.text)
+                article_content = []
+                for div in soup.findAll('div', {'class': 'bVxWXz'}):
+                    for element in div.findChildren():
+                        if AcriticaCrawler.tag_visible(element) and not element.findChild() and element.text:
+                            article_content.append(element.text)
 
-            article_data['content'] = '\n'.join(article_content).upper()
-            AcriticaCrawler.save_article_html(f"html/{article_data['article_id']}.html", soup.prettify())
-            articles.append(AcriticaParser.parse_article(article_data))
+                article_data['content'] = '\n'.join(article_content).upper()
+                AcriticaCrawler.save_article_html(f"html/{article_data['article_id']}.html", soup.prettify())
+                articles.append(AcriticaParser.parse_article(article_data))
+            except Exception as err:
+                print(str(err))
         browser.close()
         return articles
 
--- main.py
+++ main.py
@@ -3,20 +3,31 @@ from crawler import AcriticaCrawler
 from database import DatabaseManager, ArticleController
 from model import AcriticaParser
 
+
+search_words = [
+    'HAITIANO',
+    'HAITIANOS'
+    # 'HAITIANA',
+    # 'HAITIANAS',
+    # 'HAITI'
+    # 'VENEZUELANO',
+    # 'VENEZUELANOS',
+    # 'VENEZUELANA',
+    # 'VENEZUELANAS'
+    # 'VENEZUELA'
+]
 if __name__ == '__main__':
     DatabaseManager.create_database(DatabaseManager.POSTGRESQL_DB)
-    # db_links = [x.article_url for x in ArticleController.fetch_all()]
-    # news_links = AcriticaCrawler.find_news(['haitianos'])
-    # articles = AcriticaCrawler.get_articles([x for x in news_links if str(x) not in db_links])
-    articles = AcriticaCrawler.get_articles(['/geral/familias-fogem-de-casa-durante-onda-de-violencia-no-haiti-1.268772',
-                                             '/manaus/haitiana-vitima-de-perseguic-o-e-roubos-em-manaus-desabafa-me-deixem-trabalhar-em-paz-1.269209'])
-    # properties_name = set()
+
+    db_links = [x.article_url for x in ArticleController.fetch_all()]
+    news_links = AcriticaCrawler.find_news(search_words)
+    articles = AcriticaCrawler.get_articles([x for x in news_links if str(x) not in db_links])
+
     for art in articles:
         print(art)
-        try:
-            ArticleController.insert_one(art)
-        except Exception as err:
-            print(err)
-            # properties_name.add(key)
         print('\n')
-    # print(properties_name)
+
+    try:
+        ArticleController.insert_batch(articles)
+    except Exception as err:
+        print(err)
